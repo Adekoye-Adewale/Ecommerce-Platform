43000513e45f20c950159d7a84fd13fb69aaaafe
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-04-29 16:09:54 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import { getCurrentSession } from "@/actions/auth";
+import SalesCampaignBanner from "@/components/layouts/salesCampaignBanner";
+import ProductGrid from "@/components/product/productGrid";
+import { getAllProducts } from "@/sanity/lib/client";
 
 export default async function Home() {
 
   const { user } = await getCurrentSession();
 
+  const products = await getAllProducts();
+
   return (
     <div>
-      Home page
-      {JSON.stringify(user)}
+      <SalesCampaignBanner/>
+      <section>
+        <ProductGrid products={products}/>
+      </section>
     </div>
   );
 }
