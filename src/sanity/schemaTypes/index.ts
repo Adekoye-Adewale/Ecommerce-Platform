f75c8d7fa4a9379eab61574c3f334c0355ad3e5b
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-04-29 16:10:30 +0100

    Update index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,14 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { product } from './schemas/products'
+import { productCategory } from './schemas/product-categfory'
+import { promotionCampaign } from './schemas/promotion-campaign'
+import { promotionCode } from './schemas/promotion-codes'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [
+    product,
+    productCategory,
+    promotionCampaign,
+    promotionCode
+  ],
 }
